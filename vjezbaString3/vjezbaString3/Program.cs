9d33b23022bdcc9923f7b99e048beca25f95089d
LukaTepsic <166146790+LukaTepsic@users.noreply.github.com>
2024-10-28 13:09:14 +0100

    Add files via upload

---
+++ vjezbaString3/vjezbaString3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Unesite rečenicu (ili 'izlaz' za prekid):");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "izlaz")
+            {
+                break;
+            }
+
+            string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (words.Length > 0)
+            {
+                Console.WriteLine($"Prva riječ: {words[0]}");
+                Console.WriteLine($"Zadnja riječ: {words[words.Length - 1]}");
+            }
+            else
+            {
+                Console.WriteLine("Niste unijeli nijednu riječ.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
